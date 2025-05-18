929b8ba7229b423a4ed08831bed1cd59cd644a38
CakraSera <rakelsera2001@gmail.com>
2025-05-18 21:06:32 +0700

    refactor: encapsulate project rendering logic in a dedicated function

--- projects/script.js
+++ projects/script.js
@@ -3,34 +3,37 @@ console.log("🚀 ~ projects:", projects);
 
 const projectsElement = document.getElementById("all-projects");
 
-projectsElement.innerHTML = projects.map((project) => {
-  return `
-              <div class="project-card">
-              <div class="project-img">
-                <img
-                  src="/images/projects/narai-coder.jpg"
-                  alt="Project 1"
-                  width="336"
-                  height="336" />
-              </div>
-              <div class="card-content">
-                <h2>${project.name}</h2>
-                <p>
-                  ${project.description}
-                </p>
-                <div class="tech-stacks">
-                  <ul class="flex justify-center">
-                  ${
-                    project.tags
-                      ? project.tags.map((tag) => `<li>${tag}</li>`).join("")
-                      : ""
-                  }
-                  </ul>
+function renderProjects() {
+  projectsElement.innerHTML = projects.map((project) => {
+    return `
+                <div class="project-card">
+                <div class="project-img">
+                  <img
+                    src="/images/projects/narai-coder.jpg"
+                    alt="Project 1"
+                    width="336"
+                    height="336" />
                 </div>
-                <a href="${
-                  project.link
-                }" target="_blank"><button>See More Detail</button></a>
+                <div class="card-content">
+                  <h2>${project.name}</h2>
+                  <p>
+                    ${project.description}
+                  </p>
+                  <div class="tech-stacks">
+                    <ul class="flex justify-center">
+                    ${
+                      project.tags
+                        ? project.tags.map((tag) => `<li>${tag}</li>`).join("")
+                        : ""
+                    }
+                    </ul>
+                  </div>
+                  <a href="${
+                    project.link
+                  }" target="_blank"><button>See More Detail</button></a>
+                </div>
               </div>
-            </div>
-  `;
-});
+    `;
+  });
+}
+renderProjects();
