44ecbbdf3edea83a03162a324271d49706a2f348
CakraSera <rakelsera2001@gmail.com>
2025-05-18 21:02:45 +0700

    feat: update project and work experience data structure, enhance rendering logic in scripts

--- js/dummyData.js
+++ js/dummyData.js
@@ -1,4 +1,4 @@
-const projectsData = [
+export const projects = [
   {
     id: 1,
     name: "Address Book",
@@ -65,38 +65,47 @@ const projectsData = [
   },
 ];
 
-const workExperienceData = [
+export const workExperience = [
   {
     id: 1,
-    company: "Tech Company",
-    position: "Software Engineer",
-    startDate: "2022-01-01",
-    endDate: "2023-01-01",
-    description:
-      "Worked on various web development projects using JavaScript and React.",
+    companyName: "DataOn (PT Indodev Niaga Internet)",
+    position: "Implementation Consultant Jr",
+    logo: "./images/dataon-logo.png",
+    startDate: new Date("2022-01-01"),
+    endDate: new Date(),
     technologies: [
+      { name: "CFML", icon: "https://via.placeholder.com/20" },
+      { name: "React JS", icon: "https://via.placeholder.com/20" },
+      { name: "HTML", icon: "https://via.placeholder.com/20" },
+      { name: "CSS", icon: "https://via.placeholder.com/20" },
       { name: "JavaScript", icon: "https://via.placeholder.com/20" },
-      { name: "React", icon: "https://via.placeholder.com/20" },
-      { name: "Node.js", icon: "https://via.placeholder.com/20" },
+      { name: "Git", icon: "https://via.placeholder.com/20" },
+    ],
+    jobDescription: [
+      "Developing and implementing custom web applications using React.js, CFML, HTML, CSS, and JavaScript to meet specific client requirements and maintained web applications",
+      "Collaborating closely with Implementation Consultants to ensure deliverables align with client expectations and business objectives",
+      "Maintaining consistent code quality through Git version control, Gitlab for CI/CD, and SonarQube for code quality assurance",
+      "Adapting and extending DataOn's core applications to create customized solutions for enterprise clients",
+      "Troubleshooting and resolving technical issues to ensure smooth application deployment and operation",
+      "Consistently achieving high performance ratings, with a current score of 4.32/5",
     ],
   },
   {
     id: 2,
-    company: "Another Tech Company",
-    position: "Frontend Developer",
-    startDate: "2021-01-01",
-    endDate: "2022-01-01",
-    description:
-      "Developed user interfaces for web applications using HTML, CSS, and JavaScript.",
+    companyName: "PT. QATROS TEKNOLOGI NUSANTARA",
+    position: "Frontend Web Engineer",
+    logo: "./images/qatros-logo.png",
+    startDate: new Date("2022-08-01"),
+    endDate: new Date("2022-12-01"),
     technologies: [
-      { name: "HTML", icon: "https://via.placeholder.com/20" },
-      { name: "CSS", icon: "https://via.placeholder.com/20" },
-      { name: "JavaScript", icon: "https://via.placeholder.com/20" },
+      { name: "Nuxt.js", icon: "https://via.placeholder.com/20" },
+      { name: "Vue.js", icon: "https://via.placeholder.com/20" },
+      { name: "Vuex", icon: "https://via.placeholder.com/20" },
     ],
     jobDescription: [
-      "Collaborated with designers to implement UI/UX designs",
-      "Optimized web applications for maximum speed and scalability",
-      "Participated in code reviews and contributed to team knowledge sharing",
+      "Developed of high-performance web applications utilizing Nuxt.js, covering the application of core Vue.js concepts such as Vue Router, Vuex, and data reactivity",
+      "Developed website interface with booking system and Midtrans payment integration",
+      "Instrumental in building the Gompu application, applying the Scrum method in a collaborative team environment",
     ],
   },
 ];
