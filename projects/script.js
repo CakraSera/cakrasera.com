b0e1c587cdd57c935227d5f347daddcdd304454b
CakraSera <rakelsera2001@gmail.com>
2025-05-19 22:57:11 +0700

    feat: enhance contact and projects sections with improved layout and functionality

--- projects/script.js
+++ projects/script.js
@@ -4,17 +4,18 @@ console.log("🚀 ~ projects:", projects);
 const projectsElement = document.getElementById("all-projects");
 
 function renderProjects() {
-  projectsElement.innerHTML = projects.map((project) => {
-    return `
+  projectsElement.innerHTML = projects
+    .map((project) => {
+      return `
                 <div class="project-card">
                 <div class="project-img">
                   <img
-                    src="/images/projects/narai-coder.jpg"
+                    src="${project.image}"
                     alt="Project 1"
                     width="336"
                     height="336" />
                 </div>
-                <div class="card-content">
+                <div class="card-content text-center">
                   <h2>${project.name}</h2>
                   <p>
                     ${project.description}
@@ -28,12 +29,18 @@ function renderProjects() {
                     }
                     </ul>
                   </div>
-                  <a href="${
-                    project.link
-                  }" target="_blank"><button>See More Detail</button></a>
+                  <div class="!pt-2 flex gap-2.5 justify-center">
+                    <a href="${
+                      project.link
+                    }" target="_blank"><button>Live Demo</button></a>
+                    <a href="${
+                      project.github
+                    }" target="_blank"><button>Github</button></a>
+                  </div>
                 </div>
               </div>
     `;
-  });
+    })
+    .join("");
 }
 renderProjects();
